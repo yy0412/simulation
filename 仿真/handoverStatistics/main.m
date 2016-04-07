4d939896460d485d9763587b7d3e9750ee856fbf
lyy <l_yy@live.com>
2016-04-07 20:16:56 +0800

    画一个用户的运动轨迹和原服务基站和目标服务基站

--- 仿真/handoverStatistics/main.m
+++ 仿真/handoverStatistics/main.m
@@ -1,5 +1,4 @@
 %initial the parameters of network
-record_time=0;                %record the number of interval time of system
 [pois,poisofRoamingResidents,gates,restaurants,shops,offices,flats]=PIOArrange();
 [UG,W]=calculateWeight(pois);
 %部署微微基站和家庭基站
@@ -10,7 +9,8 @@ numberOfUsers=60;%应该是60个
 users=userMake(numberOfUsers,flats);
 %初始化切换统计矩阵
 count_handover_number=zeros(numberofHeNB);
-for u=1:numberOfUsers
+for u=1:numberOfUsers-59
+    record_time=0;%record the number of interval time of user
     %选择服务基站
     [theServiceBaseStation,class]=findtheServiceBaseStation(users(u,:),eNBs,HeNBs);
     Path=howtoMove(users(u,:),u,UG,pois,poisofRoamingResidents,gates,restaurants,shops,offices,flats);
@@ -20,11 +20,17 @@ for u=1:numberOfUsers
         while PathSize>0
             des=Path(1);
             point=pois(des,:);
+            plot(users(u,1),users(u,2),'Marker','s','Color','r');
             while users(u,1)~=point(1)||users(u,2)~=point(2)
                 users(u,:)=Manhattan_mobility_model(users(u,:),point);
                 %重新选择服务基站
                 [theServiceBaseStationNew,classNew]=findtheServiceBaseStation(users(u,:),eNBs,HeNBs);
-                if theServiceBaseStationNew==theServiceBaseStation%&&strcmp(class,'HeNBs')&&strcmp(classNew,'HeNBs')%发生切换
+                if theServiceBaseStationNew~=theServiceBaseStation&&strcmp(class,'HeNBs')&&strcmp(classNew,'HeNBs')%发生切换
+                    x1=HeNBs(theServiceBaseStation,:);
+                    y1=HeNBs(theServiceBaseStationNew,:);
+                    plot(users(u,1),users(u,2),'Marker','s','Color','r');
+                    plot(x1(1),x1(2),'Marker','d','Color','y');
+                    plot(y1(1),y1(2),'Marker','*','Color','r');
                     count_handover_number(theServiceBaseStation,theServiceBaseStationNew)=count_handover_number(theServiceBaseStation,theServiceBaseStationNew)+1;
                     count_handover_number(theServiceBaseStationNew,theServiceBaseStation)=count_handover_number(theServiceBaseStationNew,theServiceBaseStation)+1;
                     theServiceBaseStation=theServiceBaseStationNew;

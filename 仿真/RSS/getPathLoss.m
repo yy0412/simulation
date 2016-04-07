c8c9ce8be725569bcfe47f88896e46957da5d3a3
lyy <l_yy@live.com>
2016-04-07 09:26:11 +0800

    距离单位进行调整

--- 仿真/RSS/getPathLoss.m
+++ 仿真/RSS/getPathLoss.m
@@ -4,6 +4,7 @@ h_MS=1.5;
 switch scenario
     case 'eNB'% Okumura-Hata model from eNB to MS
         h_eNB=32;
+        distance=distance/1000;%公式里距离单位是km而HeNB里的距离单位是m
         alpha=(1.1*log10(f)-0.7)*h_MS-(1.56*log10(f)-0.8);%对于中小城市
         pl=69.55+26.16*log10(f)-13.82*log10(h_eNB)-alpha+(44.9-6.55*log10(h_eNB))*log10(distance);% dB
     case 'HeNB'%P.1238-8 model from HeNB to MS
